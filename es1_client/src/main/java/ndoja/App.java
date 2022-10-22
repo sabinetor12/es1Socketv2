e56e30b3d0b5f66ae6d3523a46971269239009b2
flavio <flaviondoja31@gmail.com>
2022-10-22 15:52:24 +0200

    branch v3

--- es1_client/src/main/java/ndoja/App.java
+++ es1_client/src/main/java/ndoja/App.java
@@ -9,12 +9,7 @@ public class App
     public static void main( String[] args )
     {
         Client client = new Client();
-        while(true){
         client.connetti();
         client.comunica();
-        }
-        //es1_client/target/
-        //es1_server/target/
-        
     }
 }
